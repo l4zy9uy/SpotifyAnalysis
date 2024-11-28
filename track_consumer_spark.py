28c3d2b9fed34e2e95c59e15f47bae3747abfe4c
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-11-28 17:11:40 +0700

    feat: convert json to csv

---
+++ track_consumer_spark.py
@@ -0,0 +1,72 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, concat_ws, array
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType, BooleanType
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+kafka_package = os.getenv('KAFKA_PACKAGE')
+
+# Initialize Spark Session
+spark = SparkSession.builder \
+    .appName('SpotifyTrackProcessor') \
+    .config('spark.jars.packages', kafka_package) \
+    .config('spark.executor.memory', '4g') \
+    .config('spark.driver.memory', '4g') \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("INFO")
+
+spark.conf.set("google.cloud.auth.service.account.enable", "true")
+spark.conf.set("google.cloud.auth.service.account.json.keyfile", os.getenv("JSON_KEYFILE"))
+
+
+# Define Schema for Required Fields
+track_schema = StructType([
+    StructField("album", StructType([
+        StructField("uri", StringType(), True),
+        StructField("artists", ArrayType(StructType([
+            StructField("uri", StringType(), True)
+        ])), True)
+    ]), True),
+    StructField("artists", ArrayType(StructType([
+        StructField("name", StringType(), True),
+        StructField("uri", StringType(), True)
+    ])), True),
+    StructField("available_markets", ArrayType(StringType()), True),
+    StructField("duration_ms", IntegerType(), True),
+    StructField("name", StringType(), True),
+    StructField("popularity", IntegerType(), True),
+    StructField("uri", StringType(), True)
+])
+
+# Read the JSON file
+gcs_input_path = "gs://don-result-csv/spotify_tracks.json"
+
+# Read the JSON file from GCS into a Spark DataFrame
+df = spark.read \
+    .schema(track_schema) \
+    .json(gcs_input_path, multiLine=True)
+
+# Combine all track artists and album artists into single columns
+processed_df = df.select(
+    col("name").alias("track_name"),
+    col("uri").alias("track_uri"),
+    col("popularity"),
+    col("duration_ms"),
+    col("album.uri").alias("album_uri"),
+    concat_ws(", ", col("album.artists.uri")).alias("album_artist_uris"),  # Combine all album artists URIs
+    concat_ws(", ", col("artists.name")).alias("artist_names"),            # Combine all track artist names
+    concat_ws(", ", col("artists.uri")).alias("artist_uris"),              # Combine all track artist URIs
+    concat_ws(", ", col("available_markets")).alias("available_markets")   # Convert array to string
+)
+
+# Write the processed data to a CSV file
+output_csv_path = './spotify_tracks'
+processed_df.coalesce(1).write \
+    .mode('overwrite') \
+    .option('header', 'true') \
+    .csv(output_csv_path)
+
+print(f"Processed data written to {output_csv_path}")
